5c91cc494837c163fddd0e2b54621e91c223a06f
Brad <bradleyasu@gmail.com>
2019-06-16 11:40:28 -0400

    Initial Commit

---
+++ MergeData.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from fuzzywuzzy import fuzz
+
+
+
+def match_names(name, list, min_score=0):
+    max_score = -1
+    max_name = ""
+    for n in list:
+
+        score = fuzz.ratio(name, n)
+
+        if (score > min_score) & (score > max_score):
+            max_name = n
+            max_score = score
+    return (max_name, max_score)
+
+
+
+
+# Import Finance Data
+df_fin_address = pd.read_csv("data/finance/factset__ent_entity_address.csv")
+df_fin_coverage = pd.read_csv("data/finance/factset__ent_entity_coverage.csv")
+df_fin_structure = pd.read_csv("data/finance/factset__ent_entity_structure.csv")
+    
+# Import Procurement Data
+
+df_pro_geo = pd.read_csv("data/procurement/mdl__dim_geo.csv")
+df_pro_vendor = pd.read_csv("data/procurement/mdl__dim_vendor.csv")
+
+
+# Combine Finance Data
+finance_data = df_fin_structure.merge(df_fin_address, left_on='factset_entity_id', right_on='factset_entity_id', how='outer').merge(df_fin_coverage, left_on='factset_entity_id', right_on='factset_entity_id', how='outer')
+
+procurement_data = df_pro_geo.merge(df_pro_vendor, left_on='geo_id', right_on='geo_id', how='outer')
+
+
+for name in procurement_data.name:
+    match = match_names(name, finance_data.entity_name)
+    print name + " --> " + str(match)
+
+
